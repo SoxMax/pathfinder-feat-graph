6bbb0bba61459f25f3d330ef8df6e03f9f8774a4
SoxMax <irishsoxmax@gmail.com>
2024-03-09 22:12:41 -0500

    add style

--- code.js
+++ code.js
@@ -1,4 +1,8 @@
 const cy = cytoscape({
   container: document.getElementById('cy'), // container to render in
-  elements: fetch('data.json').then(res => res.json())
+  elements: fetch('data.json').then(res => res.json()),
+  style: fetch('cy-style.json').then(res => res.json()),
+  layout: { name: 'grid' },
+  autoungrabify: true
 })
+
