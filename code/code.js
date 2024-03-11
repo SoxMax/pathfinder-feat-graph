167c1c808a4401ffdaf9d3004b51fbd7123c8563
SoxMax <irishsoxmax@gmail.com>
2024-03-11 10:40:03 -0400

    formatting

--- code/code.js
+++ code/code.js
@@ -4,53 +4,53 @@ const cy = cytoscape({
   style: fetch('style/cy-style.json').then(res => res.json()),
   layout: { name: 'grid' },
   autoungrabify: true
-})
+});
 
 function searchFeats(featName) {
-  const feats = cy.nodes(`[name @*= '${featName}']`)
-  const feat = feats.first()
-  const featNeighbors = feat.predecessors().union(feat.successors()).union(feat)
-  cy.nodes().removeClass('visible')
-  featNeighbors.nodes().addClass('visible')
+  const feats = cy.nodes(`[name @*= '${featName}']`);
+  const feat = feats.first();
+  const featNeighbors = feat.predecessors().union(feat.successors()).union(feat);
+  cy.nodes().removeClass('visible');
+  featNeighbors.nodes().addClass('visible');
   featNeighbors.layout({
     name: 'dagre',
     rankDir: 'LR',
     nodeSep: 50,
     rankSep: 150,
     nodeDimensionsIncludeLabels: true,
-  }).run()
+  }).run();
 }
 
 function setFeatSection(featData, section) {
-  const htmlSection = document.getElementById(`feat-${section}-section`)
-  const htmlText = document.getElementById(`feat-${section}`)
-  const sectionEntry = featData[section]
-  if(sectionEntry) {
-    htmlText.textContent = sectionEntry
-    htmlSection.hidden = false
+  const htmlSection = document.getElementById(`feat-${section}-section`);
+  const htmlText = document.getElementById(`feat-${section}`);
+  const sectionEntry = featData[section];
+  if (sectionEntry) {
+    htmlText.textContent = sectionEntry;
+    htmlSection.hidden = false;
   } else {
-    htmlSection.hidden = true
+    htmlSection.hidden = true;
   }
 }
 
 function displayFeat(featNode) {
-  const featData = featNode.data()
-  document.getElementById('feat-name').textContent = featData.name
-  setFeatSection(featData, 'description')
-  setFeatSection(featData, 'prerequisites')
-  setFeatSection(featData, 'benefit')
-  setFeatSection(featData, 'normal')
-  setFeatSection(featData, 'special')
+  const featData = featNode.data();
+  document.getElementById('feat-name').textContent = featData.name;
+  setFeatSection(featData, 'description');
+  setFeatSection(featData, 'prerequisites');
+  setFeatSection(featData, 'benefit');
+  setFeatSection(featData, 'normal');
+  setFeatSection(featData, 'special');
 }
 
 document.getElementById('search').addEventListener("change", event => {
-  searchFeats(event.target.value)
+  searchFeats(event.target.value);
 })
 
-cy.on('select', 'node', function(event){
-  displayFeat(event.target)
+cy.on('select', 'node', function (event) {
+  displayFeat(event.target);
 });
 
 cy.ready(event => {
-  console.log("Graph Loaded!")
+  console.log("Graph Loaded!");
 })
--- code/initializeData.js
+++ code/initializeData.js
@@ -4,64 +4,64 @@ String.prototype.toCamelCase = function toCamelCase() {
 
 const cy = cytoscape({
   container: document.getElementById('cy'), // container to render in
-})
+});
 
 function prerequisiteId(prerequisite) {
   return prerequisite.trim()
     .replace(/ *\([^)]*\) */g, "") //removes paren content //camel case
-    .toCamelCase()
+    .toCamelCase();
 }
 
 async function initializeGraph() {
-  const feats = await fetch('data/Feats-19Jan2020.json').then(res => res.json())
+  const feats = await fetch('data/Feats-19Jan2020.json').then(res => res.json());
   const nodes = feats.filter(feat => feat.type.toLowerCase() != "mythic")
     .map(feat => {
-      feat.id = feat.name.toCamelCase()
-      feat.prerequisite_feats = feat.prerequisite_feats.split(/(?:,|\|)+/).map(prereq => prerequisiteId(prereq)).filter(prereq => prereq)
-      delete feat.style
-      delete feat.source
-      return feat
-    })
-  const links = nodes.flatMap(feat => (feat.prerequisite_feats.map(prereq => ({ id: feat.id + '|' + prereq, source: prereq, target: feat.id }))))
+      feat.id = feat.name.toCamelCase();
+      feat.prerequisite_feats = feat.prerequisite_feats.split(/(?:,|\|)+/).map(prereq => prerequisiteId(prereq)).filter(prereq => prereq);
+      delete feat.style;
+      delete feat.source;
+      return feat;
+    });
+  const links = nodes.flatMap(feat => (feat.prerequisite_feats.map(prereq => ({ id: feat.id + '|' + prereq, source: prereq, target: feat.id }))));
 
-  const duplicates = Map.groupBy(nodes, ({ id }) => id).entries().toArray().filter(entry => entry[1].length > 1)
-  console.log("Duplicates", duplicates)
-  cy.add({ group: 'nodes', data: { id: "weaponProficiency", name: "Weapon Proficiency" } })
-  cy.add(nodes.map(node => ({ group: 'nodes', data: node })))
-  cy.add(links.map(link => ({ group: 'edges', data: link })))
+  const duplicates = Map.groupBy(nodes, ({ id }) => id).entries().toArray().filter(entry => entry[1].length > 1);
+  console.log("Duplicates", duplicates);
+  cy.add({ group: 'nodes', data: { id: "weaponProficiency", name: "Weapon Proficiency" } });
+  cy.add(nodes.map(node => ({ group: 'nodes', data: node })));
+  cy.add(links.map(link => ({ group: 'edges', data: link })));
 }
 
 function pruneNode(node) {
   const incomers = node.incomers()
   const depdeps = new Set()
   incomers.nodes().forEach(incomingFeat => {
-    incomingFeat.incomers().nodes().forEach(ele => depdeps.add(ele.data('id')))
+    incomingFeat.incomers().nodes().forEach(ele => depdeps.add(ele.data('id')));
   })
   if (depdeps.size > 0) {
-    const dupeEdges = incomers.filter(ele => ele.isEdge() && depdeps.has(ele.data('source'))).remove()
-    incomers.difference(dupeEdges).forEach(remainingNode => pruneNode(remainingNode))
+    const dupeEdges = incomers.filter(ele => ele.isEdge() && depdeps.has(ele.data('source'))).remove();
+    incomers.difference(dupeEdges).forEach(remainingNode => pruneNode(remainingNode));
   }
 }
 
 function pruneLinks() {
-  cy.nodes().leaves().forEach(leaf => pruneNode(leaf))
-  // cy.elements().addClass('visible').layout({name: 'concentric'}).run()
+  cy.nodes().leaves().forEach(leaf => pruneNode(leaf));
+  // cy.elements().addClass('visible').layout({name: 'concentric'}).run();
 }
 
 function minimumExport(cyExport) {
   return {
-    nodes: cyExport.elements.nodes.map(ele => ({data: ele.data})),
-    edges: cyExport.elements.edges.map(ele => ({data: ele.data}))
-  }
+    nodes: cyExport.elements.nodes.map(ele => ({ data: ele.data })),
+    edges: cyExport.elements.edges.map(ele => ({ data: ele.data }))
+  };
 }
 
 async function main() {
-  await initializeGraph()
-  const initialized = minimumExport(cy.json(false))
-  console.log("Initialized", initialized)
-  pruneLinks()
-  const pruned = minimumExport(cy.json(false))
-  console.log("Pruned", pruned)
+  await initializeGraph();
+  const initialized = minimumExport(cy.json(false));
+  console.log("Initialized", initialized);
+  pruneLinks();
+  const pruned = minimumExport(cy.json(false));
+  console.log("Pruned", pruned);
 }
 
-main()
+main();
